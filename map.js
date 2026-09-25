64302375f9c5caa530e130af6b0a9e10fcaa1eeb
Ikimonomap08 <asay9515@gmail.com>
2026-09-25 22:40:14 +0900

    Update map.js

--- map.js
+++ map.js
@@ -22,7 +22,7 @@ const supabaseClient =
 // 地図の作成
 // =====================================
 
-const map = L.map("map").setView([34.5, 136.8], 9);
+const map = L.map("map").setView([34.45, 136.08], 14);
 
 L.tileLayer(
   "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
